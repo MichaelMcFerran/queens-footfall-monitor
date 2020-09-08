14b3f123a70520dd83e0ee8adfea75bc8e4ceff8
Michael <mmcferran628@qub.ac.uk>
2020-09-08 20:44:38 +0100

    testing Windows Dev environment

--- views/index.php
+++ views/index.php
@@ -1,28 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Door Count Demo</title>
+  <title>Door Count Demo</title>
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+  <!-- CSS -->
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+  <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
 
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+  <!-- Custom styles -->
+  <link href="/stylesheets/style.css" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="/stylesheets/style.css" rel="stylesheet">
+  <!-- ****** faviconit.com favicons ****** -->
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="icon" sizes="16x16 32x32 64x64" href="img/faviconit/favicon.ico">
+  <link rel="icon" type="image/png" sizes="196x196" href="img/faviconit/favicon-192.png">
+  <link rel="icon" type="image/png" sizes="160x160" href="img/faviconit/favicon-160.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="img/faviconit/favicon-96.png">
+  <link rel="icon" type="image/png" sizes="64x64" href="img/faviconit/favicon-64.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="img/faviconit/favicon-32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="img/faviconit/favicon-16.png">
+  <link rel="apple-touch-icon" href="img/faviconit/favicon-57.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="img/faviconit/favicon-114.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="img/faviconit/favicon-72.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="img/faviconit/favicon-144.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="img/faviconit/favicon-60.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="img/faviconit/favicon-120.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="img/faviconit/favicon-76.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="img/faviconit/favicon-152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="img/faviconit/favicon-180.png">
+  <meta name="msapplication-TileColor" content="#FFFFFF">
+  <meta name="msapplication-TileImage" content="img/faviconit/favicon-144.png">
+  <meta name="msapplication-config" content="img/faviconit/browserconfig.xml">
+  <!-- ****** faviconit.com favicons ****** -->
 
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
+</head>
 
   <body>
 
