c78dfb494c4cb62c29929c5607379b54f182db77
Michael <mmcferran628@qub.ac.uk>
2020-09-09 16:09:15 +0100

    layout.jade

---
+++ routes/monitor.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET monitor page. */
+router.get('/monitor', function(req, res) {
+    res.render('monitor', { title: 'Express' });
+  });
+
+
+
+
+module.exports = router;
+
