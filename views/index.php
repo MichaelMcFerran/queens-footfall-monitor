b926ef7aa7a044d01c28f595b28c8d468ee8a755
Michael <mmcferran628@qub.ac.uk>
2020-09-09 17:50:30 +0100

    sds

--- views/index.php
+++ views/index.php
@@ -16,26 +16,26 @@
 
  <!-- Queens logo fix -->
     <!-- ****** faviconit.com favicons ****** -->
-  <link rel="shortcut icon" href="/img/favicon.ico">
-  <link rel="icon" sizes="16x16 32x32 64x64" href="/img/faviconit/favicon.ico">
-  <link rel="icon" type="image/png" sizes="196x196" href="/img/faviconit/favicon-192.png">
-  <link rel="icon" type="image/png" sizes="160x160" href="/img/faviconit/favicon-160.png">
-  <link rel="icon" type="image/png" sizes="96x96" href="/img/faviconit/favicon-96.png">
-  <link rel="icon" type="image/png" sizes="64x64" href="/img/faviconit/favicon-64.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="/img/faviconit/favicon-32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="/img/faviconit/favicon-16.png">
-  <link rel="apple-touch-icon" href="/img/faviconit/favicon-57.png">
-  <link rel="apple-touch-icon" sizes="114x114" href="/img/faviconit/favicon-114.png">
-  <link rel="apple-touch-icon" sizes="72x72" href="/img/faviconit/favicon-72.png">
-  <link rel="apple-touch-icon" sizes="144x144" href="/img/faviconit/favicon-144.png">
-  <link rel="apple-touch-icon" sizes="60x60" href="/img/faviconit/favicon-60.png">
-  <link rel="apple-touch-icon" sizes="120x120" href="/img/faviconit/favicon-120.png">
-  <link rel="apple-touch-icon" sizes="76x76" href="/img/faviconit/favicon-76.png">
-  <link rel="apple-touch-icon" sizes="152x152" href="/img/faviconit/favicon-152.png">
-  <link rel="apple-touch-icon" sizes="180x180" href="/img/faviconit/favicon-180.png">
+  <link rel="shortcut icon" href="/public/img/favicon.ico">
+  <link rel="icon" sizes="16x16 32x32 64x64" href="/public/img/faviconit/favicon.ico">
+  <link rel="icon" type="image/png" sizes="196x196" href="/public/img/faviconit/favicon-192.png">
+  <link rel="icon" type="image/png" sizes="160x160" href="/public/img/faviconit/favicon-160.png">
+  <link rel="icon" type="image/png" sizes="96x96" href="/public/img/faviconit/favicon-96.png">
+  <link rel="icon" type="image/png" sizes="64x64" href="/public/img/faviconit/favicon-64.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="/public/img/faviconit/favicon-32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="/public/img/faviconit/favicon-16.png">
+  <link rel="apple-touch-icon" href="/public/img/faviconit/favicon-57.png">
+  <link rel="apple-touch-icon" sizes="114x114" href="/public/img/faviconit/favicon-114.png">
+  <link rel="apple-touch-icon" sizes="72x72" href="/public/img/faviconit/favicon-72.png">
+  <link rel="apple-touch-icon" sizes="144x144" href="/public/img/faviconit/favicon-144.png">
+  <link rel="apple-touch-icon" sizes="60x60" href="/public/img/faviconit/favicon-60.png">
+  <link rel="apple-touch-icon" sizes="120x120" href="/public/img/faviconit/favicon-120.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="/public/img/faviconit/favicon-76.png">
+  <link rel="apple-touch-icon" sizes="152x152" href="/public/img/faviconit/favicon-152.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="/public/img/faviconit/favicon-180.png">
   <meta name="msapplication-TileColor" content="#FFFFFF">
-  <meta name="msapplication-TileImage" content="/img/faviconit/favicon-144.png">
-  <meta name="msapplication-config" content="/img/faviconit/browserconfig.xml">
+  <meta name="msapplication-TileImage" content="/public/img/faviconit/favicon-144.png">
+  <meta name="msapplication-config" content="/public/img/faviconit/browserconfig.xml">
   <!-- ****** faviconit.com favicons ****** -->
 
 </head>
