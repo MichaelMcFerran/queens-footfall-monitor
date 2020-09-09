1dbb96dcc35adbd780c757355927a8d241316aa9
Michael <mmcferran628@qub.ac.uk>
2020-09-09 15:31:19 +0100

    render monitor page

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,11 @@ var router = express.Router();
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
+
 /* GET monitor page. */
 router.get('/monitor.php', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('monitor', { title: 'Express' });
 });
+
 
 module.exports = router;
