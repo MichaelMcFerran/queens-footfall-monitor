7a7fc715eff0ba56fe6211f4b11a35afb7501b52
Michael <mmcferran628@qub.ac.uk>
2020-09-10 20:19:22 +0100

    test

--- app.js
+++ app.js
@@ -9,8 +9,24 @@ var routes = require('./routes/index');
 
 var app = express();
 
-//hopefully should all conn.php to connect  https://www.npmjs.com/package/php
-var php = require('php');
+// //doesnt work testing running php in node.js
+// var execPHP = require('./execphp.js')();
+// //maybe change to separate folder for just php files
+// execPHP.phpFolder = 'C:\\Users\\Michael Mc Ferran\\Documents\\MSc SoftwareDev\\MastersProject\\queens-footfall-monitor\\views\\';
+
+// app.use('*.php',function(request,response,next) {
+// 	execPHP.parseFile(request.originalUrl,function(phpResult) {
+// 		response.write(phpResult);
+// 		response.end();
+// 	});
+// });
+
+// app.listen(3000, function () {
+// 	console.log('Node server listening on port 3000!');
+// });
+
+// DOESNT WORK hopefully should all conn.php to connect  https://www.npmjs.com/package/php
+//var php = require('php');
 
 //connection to db test
 var mysql = require('mysql');
@@ -22,23 +38,47 @@ var con = mysql.createConnection({
   database : 'heroku_58f73cf4b46766d'
 });
 
+// //check connection
+// con.connect(function (err) {
+//     if (err) throw err;
+//     console.log("Connected!");
+// });
+
+//unused atm
 con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM FMusers", function (err, result, fields) {
+    var log = "SELECT * FROM FMusers"
+    con.query(log, function (err, result, fields) {
       if (err) throw err;
       var resultLo = result;
       console.log(resultLo);
     });
   });
 
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+//pointless, works with test.js
+// module.exports  = {
+//     select: function (callback) {
+//         var log = "SELECT * FROM FMusers";
+//         con.query(log, function (err, result, fields) {
+//         if (err) throw err;
+//         var resultLo = result;
+//         console.log(resultLo);
+//         });
+//     }
+// }
 
-// setup php templating engine https://www.npmjs.com/package/php
+
+
+
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'php');
-app.engine('php', php.__express);
+app.set('view engine', 'jade');
+
+// setup php templating engine https://www.npmjs.com/package/php doesn't work
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'php');
+// app.engine('php', php.__express);
 
 app.use(favicon());
 //fix to get favicon working https://stackoverflow.com/questions/15463199/how-to-set-custom-favicon-in-express
