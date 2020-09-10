59286ed712951304c529721b5244ac7b77b0c67d
Michael <mmcferran628@qub.ac.uk>
2020-09-10 15:19:03 +0100

    fixing routin

--- routes/monitor.js
+++ routes/monitor.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET monitor page. */
 router.get('/monitor', function(req, res) {
-    res.render('monitor', { title: 'Express' });
+    // res.render('monitor', { title: 'Express' });
+    //added in own layout page to find separate css 
+    res.render('monitor', { layout: 'monitorlayout.jade', title: 'Express' });
   });
 
 
