50d14736697bfc0eb1a4fa6239114b46f6008531
Michael <mmcferran628@qub.ac.uk>
2020-10-05 01:31:39 +0100

    file deletion

--- routes/index.js
+++ routes/index.js
@@ -8,21 +8,10 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET monitor page. */
-router.get('/monitor', function(req, res, mysql) {
-  // res.render('monitor', { title: 'Express' });
-  //added in own layout page to find separate css 
-  res.render('monitor', { layout: 'monitorlayout.jade', title: 'Express' });
-});
 
 /* GET connection to dB */
 router.get('/conn.php', function(req, res) { //should it be /conn.php
   res.render('conn', { title: 'Express' });
-});
-
-/* GET test.html */
-router.get('/test', function(req, res) { //should it be /conn.php
-  res.render('test', { title: 'Express' });
 });
 
 
